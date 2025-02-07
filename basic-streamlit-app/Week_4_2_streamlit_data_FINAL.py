e6b741c1dce7c1a364fb9a27803367463fd47ede
Cecilia Pino <cpino2@nd.edu>
2025-02-07 16:16:58 -0500

    population data uploading csv file

--- basic-streamlit-app/Week_4_2_streamlit_data_FINAL.py
+++ basic-streamlit-app/Week_4_2_streamlit_data_FINAL.py
@@ -41,7 +41,7 @@ st.dataframe(filtered_df)
 
 # Now, instead of creating a DataFrame manually, we load a CSV file
 # This teaches students how to work with external data in Streamlit
-df = pd.read_csv("data\penguins.csv")  # Ensure the "data" folder exists with the CSV file
+df = pd.read_csv("data\population.csv")  # Ensure the "data" folder exists with the CSV file
 # Display the imported dataset
 st.write("Here's the dataset loaded from a CSV file:")
 st.dataframe(df) #this lets you have access to the dataframe inside of your data folder
